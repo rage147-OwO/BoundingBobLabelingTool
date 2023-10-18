d7126d9429a9dc39f3aa0a5d42f05d2a6dc10636
suhyun-kim-div <86995809+suhyun-kim-div@users.noreply.github.com>
2023-10-18 15:28:55 +0900

    init

---
+++ BoundingBoxLabelingTool/MainViewModel.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace BoundingBoxLabelingTool
+{
+    internal class MainViewModel : INotifyPropertyChanged
+    {
+
+
+        private string _filePath;
+        public string FilePath
+        {
+            get { return _filePath; }
+            set
+            {
+                _filePath = value;
+                OnPropertyChanged(nameof(FilePath));
+            }
+        }
+
+
+        //바인딩, ObservableCollection은 UI에 바로 반영됨(양방향)
+        private ObservableCollection<string> _imageList = new ObservableCollection<string>();
+        public ObservableCollection<string> ImageList 
+        {
+            get { return _imageList; }
+            set
+            {
+                _imageList = value;
+                OnPropertyChanged(nameof(ImageList));
+            }
+        }
+
+
+
+        private string _selectedImagePath;
+        public string SelectedImagePath
+        {
+            get { return _selectedImagePath; }
+            set
+            {
+                _selectedImagePath = value;
+                OnPropertyChanged(nameof(SelectedImagePath));
+            }
+        }
+
+        private double _scaleRate = 1.0;
+        public double ScaleRate //바인딩
+        {
+            get { return _scaleRate; }
+            set
+            {
+                if (_scaleRate != value)
+                {
+                    _scaleRate = value;
+                    OnPropertyChanged(nameof(ScaleRate));
+                    Debug.WriteLine(_scaleRate);
+                }
+            }
+        }
+
+
+
+
+
+
+        public MainViewModel()
+        {
+
+        }
+
+
+        public void LoadDirectory()
+        {
+            var dialog = new System.Windows.Forms.FolderBrowserDialog();
+            DialogResult result = dialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                FilePath = dialog.SelectedPath;
+                string[] imageFiles = System.IO.Directory.GetFiles(FilePath, "*.png");
+                ImageList.Clear();
+                foreach (var imageFile in imageFiles)
+                {
+                    ImageList.Add(imageFile);
+
+
+                }
+
+            }
+
+
+        }
+
+        public void HandleMouseWheelScroll(MouseWheelEventArgs e)
+        {
+            if (Keyboard.Modifiers == ModifierKeys.Control)
+            {
+                double scaleMultiplier = 0.1;
+                ScaleRate = e.Delta > 0 ? ScaleRate * (1 + scaleMultiplier) : ScaleRate * (1 - scaleMultiplier);
+                Debug.WriteLine(ScaleRate.ToString());
+            }
+        }
+
+
+        public void HandleSelectionChanged(string selectedImagePath)
+        {
+            SelectedImagePath = selectedImagePath;
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void
+            OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            //Debug.WriteLine(propertyName + " was set");
+        }
+    }
+
+    internal class ImageManager
+    {
+        private string _directoryPath;
+        private List<ImageData> _imageDatas;
+        public ImageManager(string _directoryPath) {
+            this._directoryPath = _directoryPath;
+            _imageDatas = new List<ImageData>();
+
+            
+
+
+        }
+
+
+    }
+
+    public class ImageData
+    {
+
+    }
+
+
+}
---
+++ BoundingBoxLabelingTool/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace BoundingBoxLabelingTool
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private MainViewModel _viewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _viewModel = new MainViewModel();
+            DataContext = _viewModel;
+        }
+
+        private void LoadDir_Click(object sender, RoutedEventArgs e)
+        {
+            _viewModel.LoadDirectory();
+        }
+
+        private void ImageScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            _viewModel.HandleMouseWheelScroll(e);
+        }
+
+
+        private void ImageDirListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string selectedImagePath = imageDirListBox.SelectedItem.ToString();
+            _viewModel.HandleSelectionChanged(selectedImagePath);
+        }
+
+        private void Left_Click(object sender, RoutedEventArgs e)
+        {
+            if (imageDirListBox.SelectedIndex > 0)
+            {
+                imageDirListBox.SelectedIndex--;
+                _viewModel.HandleSelectionChanged(imageDirListBox.SelectedItem.ToString());
+            }
+        }
+
+        private void Right_Click(object sender, RoutedEventArgs e)
+        {
+            if (imageDirListBox.SelectedIndex < imageDirListBox.Items.Count - 1)
+            {
+                imageDirListBox.SelectedIndex++;
+                _viewModel.HandleSelectionChanged(imageDirListBox.SelectedItem.ToString());
+            }
+
+        }
+
+
+        #region Bounding Box Drawing Logic
+
+        private bool isDrawing;
+        private Point startPoint;
+        private Rect boundingBox;
+
+        private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Image_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+
+
+        private void Image_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        #endregion
+    }
+}
